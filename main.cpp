df66ac6b52212f936a9d6ea45b2e19df729c1821
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-23 01:27:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -143,7 +143,7 @@ int main() {
 	        /* open file */ 
 	
 		ofstream read;
-		read.open("SI_200815_adaptivereversecases.csv", ios_base::app);                                                         
+		read.open("SI_200823_adaptivereversetrack.csv", ios_base::app);                                                         
 		vector <double> accuracy (125,0);
 	        
 	        /* define name for sum of accuaracy for every n epochs */ 
