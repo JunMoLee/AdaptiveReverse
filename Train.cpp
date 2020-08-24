63250dd8e0a1b65539d75793aefc4954936572d1
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-25 02:54:45 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -2618,7 +2618,7 @@ cout<<"0.8<=w<=1"<<" : "<<a101<<", "<< a102<<", "<<a103<<endl;
 		
 
 		ofstream read;
-		string filename="+1-9optimizatinoprob";
+		string filename="finalprob";
 		read.open(filename+ ".csv",std::ios_base::app);
 		read << "epoch"<<", "<<epochcount<<endl;
 		read <<"IH"<<", "<< countGpweightrange/countGprange <<", "<<"HO"<<", "<<weightlocationspecifierGn/countGnrange<<endl;
@@ -2635,7 +2635,7 @@ read<<"0.8<=w<=1"<<" : "<<", "<<possaturatedweight5<<", "<<  negsaturatedweight5
 
 
 				ofstream readx;
-		string filenamey="+1-9optimizationdest";
+		string filenamey="finaldestruct";
 		readx.open(filenamey+ ".csv",std::ios_base::app);
 		readx << "epoch"<<", "<<epochcount<<endl;
 readx<<"-1<=w<-0.8"<<" : "<<", "<<a11 <<", "<<a12<<", "<<a13<<endl;
