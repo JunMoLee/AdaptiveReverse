8514c7eceb2ddef9a85ad53d0a8328608d115cbd
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2020-08-29 18:13:08 +0900

    Update Param.cpp

--- Param.cpp
+++ Param.cpp
@@ -339,65 +339,63 @@ switch(a){
 case 0:
 		
 param_gp=1;
-param_gn=-1;
+param_gn=-3;
+	learningrate[0][0] =0.3;
+	learningrate[0][1] = 0.3;
+	learningrate[0][2] = 0.3;
+	learningrate[0][3] = 0.3;
 		
-		learningrate[0][0] =0.2;
-	learningrate[0][1] = 0.2;
-	learningrate[0][2] = 0.2;
-	learningrate[0][3] = 0.2;
-		
-	learningrate[1][0] = 0.2;
-	learningrate[1][1] = 0.2;
-	learningrate[1][2] = 0.2;
-	learningrate[1][3] = 0.2;
+	learningrate[1][0] = 0.3;
+	learningrate[1][1] = 0.3;
+	learningrate[1][2] = 0.3;
+	learningrate[1][3] = 0.3;
 		newUpdateRate=2;
 		nnewUpdateRate=2;
 
 
-		RefreshRate = 4000;
+		RefreshRate = 100;
 		ReverseUpdate = 1;
 
 	FullRefresh= 0;
-	saturationprotector=0.2;
-	destructionprotector=0.18;
-	deltaweightratio=1.7;
-	usesplit = 1;
-	Gth1=1;
-	Gth2=7;
-	Gth1weight=1;
-	Gth2weight=7;
+	saturationprotector=0.8;
+	destructionprotector=0.8;
+	deltaweightratio=1.2;
+		usesplit = 1;
+	Gth1=2;
+	Gth2=8;
+	Gth1weight=2;
+	Gth2weight=8;
 	unitcellsplit=1;
 break;
 case 1:
 		
 param_gp=1;
-param_gn=-9;
-		
+param_gn=-3;
 	learningrate[0][0] =0.3;
 	learningrate[0][1] = 0.3;
-	learningrate[0][2] = 0.15;
-	learningrate[0][3] = 0.2;
+	learningrate[0][2] = 0.3;
+	learningrate[0][3] = 0.3;
 		
 	learningrate[1][0] = 0.3;
 	learningrate[1][1] = 0.3;
-	learningrate[1][2] = 0.15;
-	learningrate[1][3] = 0.2;
-		newUpdateRate=3;
-		nnewUpdateRate=3;
+	learningrate[1][2] = 0.3;
+	learningrate[1][3] = 0.3;
+		newUpdateRate=2;
+		nnewUpdateRate=2;
 
 
-		RefreshRate = 4000;
+		RefreshRate = 100;
 		ReverseUpdate = 1;
 
 	FullRefresh= 0;
-	saturationprotector=2.7;
-	destructionprotector=0;
-	deltaweightratio=1;
-	usesplit = 1;
-	Gth1=1;
-	Gth2=7;
-	Gth1weight=1;
-	Gth2weight=7;
+	saturationprotector=0.8;
+	destructionprotector=0.4;
+	deltaweightratio=1.2;
+		usesplit = 1;
+	Gth1=2;
+	Gth2=8;
+	Gth1weight=2;
+	Gth2weight=8;
 	unitcellsplit=1;
 break;
 case 2:
